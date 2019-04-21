9f91da4dc13307045c9ccf0910d98836dbcb783e
Joao Pucci <joao.pucci@live.com>
2019-04-20 22:25:34 -0300

    Adiciona classe Sabor

--- src/main/java/tech/pucci/bootstrap/BootstrapData.java
+++ src/main/java/tech/pucci/bootstrap/BootstrapData.java
@@ -5,16 +5,20 @@ import java.math.BigDecimal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import tech.pucci.model.Sabor;
 import tech.pucci.model.Tamanho;
+import tech.pucci.repository.SaborRepository;
 import tech.pucci.repository.TamanhoRepository;
 
 @Component
 public class BootstrapData implements CommandLineRunner {
 	
 	private TamanhoRepository tamanhoRepository;
+	private SaborRepository saborRepository;
 	
-	public BootstrapData(TamanhoRepository tamanhoDao) {
+	public BootstrapData(TamanhoRepository tamanhoDao, SaborRepository saborRepository) {
 		this.tamanhoRepository = tamanhoDao;
+		this.saborRepository = saborRepository;
 	}
 
 	@Override
@@ -22,9 +26,9 @@ public class BootstrapData implements CommandLineRunner {
 		
 		System.out.println("Loading initial data...");
 		cadastrarTamanhos();
+		cadastrarSabores();
 
-		
-		System.out.println(tamanhoRepository.findAll());
+		System.out.println(saborRepository.findAll());
 	}
 	
 	private void cadastrarTamanhos() {
@@ -45,6 +49,23 @@ public class BootstrapData implements CommandLineRunner {
 		tamanhoGrande.setTempoPreparo(25);
 		tamanhoGrande.setValor(BigDecimal.valueOf(40.0));
 		tamanhoRepository.save(tamanhoGrande);
+	}
+	
+	private void cadastrarSabores() {
+		Sabor saborCalabresa = new Sabor();
+		saborCalabresa.setDescricao("Calabresa");
+		saborCalabresa.setTempoAdicional(0);
+		saborRepository.save(saborCalabresa);
+	
+		Sabor saborMarguerita = new Sabor();
+		saborMarguerita.setDescricao("Marguerita");
+		saborMarguerita.setTempoAdicional(0);
+		saborRepository.save(saborMarguerita);
+		
+		Sabor saborPortuguesa = new Sabor();
+		saborPortuguesa.setDescricao("Portuguesa");
+		saborPortuguesa.setTempoAdicional(5);
+		saborRepository.save(saborPortuguesa);
 	}
 	
 }
